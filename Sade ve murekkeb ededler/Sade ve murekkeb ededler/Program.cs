41b369f9ca217f67a30ecfe6f6ca9868d3b6d0ca
hebibulla imranov <tu7q3fuwv@code.edu.az>
2022-11-01 00:15:50 +0400

    Sade ededler ve murekkeb ededler

---
+++ Sade ve murekkeb ededler/Sade ve murekkeb ededler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sade_ve_murekkeb_ededler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("number=");
+            n = int.Parse(Console.ReadLine());
+            int divisors = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors++;
+                }
+            }
+            if (divisors == 2)
+            {
+                Console.WriteLine("Verilmis eded sade ededdir");
+            }
+            else
+            {
+                Console.WriteLine("Verilmis eded murekkeb ededdir");
+            }
+            Console.ReadLine();
+        }
+    }
+}
